3f1c915feb775a145826fa3708c2fd8a410c251a
JPDPH <delpozohjp@gmail.com>
2023-04-17 21:19:49 -0500

    sesion

--- src/Pages/iniciar_sesion.js
+++ src/Pages/iniciar_sesion.js
@@ -1,6 +1,29 @@
 import BarraNavega from "./BarraNavega";
+import React, { useEffect, useState } from "react"
+import {UNSAFE_DataRouterStateContext, useNavigate } from "react-router-dom"
+
+const usuarioC = ["alberto","123", "renato","321"]
+const usuarioG = ["jesus","ola", "yaru","contra"]
 
 const Sesion = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate();
+
+    const loginOnClick = () => {
+        let passw = document.getElementById("passwor").value;
+        if(usuarioC.includes(passw) && usuarioC.includes(username)){
+            navigate("/")
+        }else if(usuarioG.includes(username) && usuarioG.includes(passw)){
+            navigate("/guia")
+        }else{
+            alert("No existe este usuario")
+        }
+
+    }
+    console.log(username)
+
     return(
         <div>
             <BarraNavega></BarraNavega>
@@ -11,16 +34,17 @@ const Sesion = () => {
                         <label className="form-label">
                             Usuario
                         </label>
-                        <input className="form-control" />
+                        <input className="form-control" value={username} onChange={(evt)=>{setUsername(evt.target.value)}}/>
                     </div>
                     <div>
-                        <label className="form-label">
+                        <label className="form-label" value={password} onChange={(evt)=>{setPassword(evt.target.value)}}>
                         Password
                         </label>
-                        <input type="password" className="form-control"  />
+                        <input id="passwor" type="password" className="form-control"  />
                     </div>
                 </div>
-                <a href="#" >Login</a>
+                <button className="btn btn-primary mt-2" type="button" onClick={loginOnClick}>Login</button>
+                
             </div>
         </div>
     );
